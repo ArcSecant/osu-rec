a1a237c38e1a5b49069dbfe74fbc62291c2e0a53
Jerry <lightlance7@gmail.com>
2020-10-12 02:03:40 -0700

    owo

--- backend/main.py
+++ backend/main.py
@@ -18,11 +18,12 @@ inv_map = {v: k for k, v in maps_dict.items()}
 
 # user_items_l = user_items.tolil()
 # for i in tqdm(range(user_items_l.shape[0])):
-#     user_items_l[i] *= ((1/user_items_l[i].sum())**0.5)
+#     user_items_l[i] *= 1/1.000005**((user_items_l[i].sum() - 200)**2)
+#     # user_items_l[i] *= 1/user_items_l[i].sum()**0.01
 # items_id = user_items_l.tocoo()
 # sparse.save_npz('backend/model/user_items_weighted.npz', items_id)
 # model = implicit.lmf.LogisticMatrixFactorization(factors=32)
-# model.fit(user_items)
+# model.fit(items_id)
 # pickle.dump(model, open('backend/model/finalized_model_weighted.sav', 'wb'))
 
 @app.route('/user', methods=['POST'])
@@ -30,7 +31,7 @@ def recommendations():
     global model, user_items, inv_map, id_list
     
     user_id = request.json['input']
-    recommendations = model.recommend(id_list.index(int(user_id)), user_items.T.tocsr(), N=25)
+    recommendations = model.recommend(id_list.index(int(user_id)), user_items.T.tocsr(), N=50)
     rec = random.choice(recommendations)[0]
 
     return {'output': 'https://osu.ppy.sh/beatmapsets/' + str(inv_map[rec])}
@@ -40,7 +41,7 @@ def similar_maps():
     global model, user_items, inv_map, id_list
 
     mapset_id = request.json['input']
-    similar_maps = model.similar_items(maps_dict[int(mapset_id)], N=25)[1:]
+    similar_maps = model.similar_items(maps_dict[int(mapset_id)], N=50)[1:]
     rec = random.choice(similar_maps)[0]
 
     return {'output': 'https://osu.ppy.sh/beatmapsets/' + str(inv_map[rec])}
